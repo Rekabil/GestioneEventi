bfaeb5849b12a3b13aaacf24899f0183d5275987
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-11-17 11:05:46 +0100

    Repository

---
+++ src/main/java/BilgenKaanRemzi/GestioneEventi/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package BilgenKaanRemzi.GestioneEventi.repository;
+
+import BilgenKaanRemzi.GestioneEventi.entieties.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Integer> {
+}
