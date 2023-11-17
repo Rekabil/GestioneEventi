20ccee12c98c498b21a077059bac5afabc45dfff
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-11-17 09:59:46 +0100

    first

---
+++ src/test/java/BilgenKaanRemzi/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package BilgenKaanRemzi.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
